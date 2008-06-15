00b49dbf4240a6012cd3f1ae8bf459a9f71854b4
mourezwell <mourezwell@users.sourceforge.net>
2008-06-15 22:02:13 +0000

    close action simplification

--- src/com/oz/lanslim/gui/MainPaneWindowListener.java
+++ src/com/oz/lanslim/gui/MainPaneWindowListener.java
@@ -4,6 +4,7 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import com.oz.lanslim.model.SlimModel;
 
@@ -19,17 +20,22 @@ public class MainPaneWindowListener extends WindowAdapter {
 
 	public void windowClosing(WindowEvent e) {
 		
-		if (model.getSettings().isCloseAsTrayForced() && model.getSettings().isTrayEnable()) {
+		if (model.getSettings().isCloseAsTray() && model.getSettings().isTrayEnable()) {
 			mainFrame.setVisible(false);
 		}
-		else if (model.getSettings().isCloseForced()) {
+		else if (!model.getSettings().isCloseAsTray()) {
 			System.exit(0);
 		}
-		else { // no choice done yet or choice disabled
-			CloseActionFrame lFrame =  new CloseActionFrame(model, mainFrame);
-			lFrame.pack();
-			lFrame.setLocationRelativeTo(mainFrame);
-			lFrame.setVisible(true);
+		else { // close As tray and tray diasabled
+			int a = JOptionPane.showConfirmDialog(mainFrame,
+				    "Tray icon is disabled, please check that systray native library (dll or so) is in the same directory as jar (exe). Are you sure you want to exit ?",
+				    "Close confirmation",
+				    JOptionPane.YES_NO_OPTION,
+				    JOptionPane.WARNING_MESSAGE);
+			
+			if (a == JOptionPane.YES_OPTION) {
+				System.exit(0);
+            }
 		}
 	}
 }
