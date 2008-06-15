18d087596ea0647650ad63c669a5e06b1f2f8283
mourezwell <mourezwell@users.sourceforge.net>
2008-06-15 22:01:58 +0000

    drag and drop improvements

---
+++ src/com/oz/lanslim/gui/DragAndDropWithoutSelection.java
@@ -0,0 +1,42 @@
+/*
+ *-------------------------------------------------------------------------------------------------
+ *   Classe : DragAndDropWithoutSelection
+ *   Copyright 2007 by AIRBUS France
+ *-------------------------------------------------------------------------------------------------
+ */
+package com.oz.lanslim.gui;
+
+import java.awt.Component;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class DragAndDropWithoutSelection {
+
+    
+    public static void hackSingleSelectionDND(Component c){
+        MouseListener dragListener = null;
+
+        // the default dnd implemntation requires to first select node and then drag
+        try{
+            Class clazz = Class.forName("javax.swing.plaf.basic.BasicDragGestureRecognizer");
+            MouseListener[] mouseListeners = c.getMouseListeners();
+            for(int i = 0; i<mouseListeners.length; i++){
+                if(clazz.isAssignableFrom(mouseListeners[i].getClass())){
+                    dragListener = mouseListeners[i];
+                    break;
+                }
+            }
+
+            if(dragListener!=null){
+                c.removeMouseListener(dragListener);
+                c.removeMouseMotionListener((MouseMotionListener)dragListener);
+                c.addMouseListener(dragListener);
+                c.addMouseMotionListener((MouseMotionListener)dragListener);
+            }
+        } catch(ClassNotFoundException e){
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+
+}
