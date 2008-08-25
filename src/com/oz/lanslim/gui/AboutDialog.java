01d913f264ffcd4798e59cb42c607fb76dafe103
mourezwell <mourezwell@users.sourceforge.net>
2008-08-25 23:17:02 +0000

    version

--- src/com/oz/lanslim/gui/AboutDialog.java
+++ src/com/oz/lanslim/gui/AboutDialog.java
@@ -9,9 +9,9 @@ import com.oz.lanslim.StringConstants;
 
 public class AboutDialog extends JOptionPane {
 	
-	public static final String VERSION = "1.01"; //$NON-NLS-1$
+	public static final String VERSION = "1.02"; //$NON-NLS-1$
 	private static final String AUTHOR_NAME = "Olivier Mourez"; //$NON-NLS-1$
-	private static final String AUTHOR_ADDRESS = "mourezwell@lanslim.sourceforge.net"; //$NON-NLS-1$
+	private static final String AUTHOR_ADDRESS = "mourezwell@users.sourceforge.net"; //$NON-NLS-1$
 	private static final String PROJECT_HOME = "http://sourceforge.net/projects/lanslim/"; //$NON-NLS-1$
 	
 	public static void showDialog(Component parentComponent) {
