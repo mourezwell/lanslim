bfbcfd9f1fa67834a36f09fe226323e43ffe5be2
mourezwell <mourezwell@users.sourceforge.net>
2009-07-11 17:39:17 +0000

    version 1.3.0 : Redesign by introducing TrayIconManager

--- src/com/oz/lanslim/SlimIconListener.java
+++ src/com/oz/lanslim/SlimIconListener.java
@@ -2,6 +2,9 @@ package com.oz.lanslim;
 
 public interface SlimIconListener {
 	
-	public void startIconBlinking();
-	
+	public void startIconBlinking(boolean pBlink, boolean pPop, String pMessage);
+
+	public void stopIconBlinking();
+
+	public void setFocus(boolean psFocus);
 }
